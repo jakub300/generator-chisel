27596d720914af68924fb5516d38a37db3af9a9f
Jakub Bogucki <jakub300@gmail.com>
2017-11-20 19:49:42 +0100

    Make version tests optional, fix ejs, run pack later

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,17 +7,18 @@ if [ "$TEST_VERSIONS" == "" ]; then
   mocha test/**/*.test.js -b --reporter spec
 elif [ "$TEST_VERSIONS" == "generator" ]; then
   sudo npm install --quiet -g nsp nsp-preprocessor-yarn npm-check > /dev/null
-  nsp check --preprocessor yarn
-  npm-check -s
+  nsp check --preprocessor yarn || (exit 0)
+  npm-check -s || (exit 0)
 elif [ "$TEST_VERSIONS" == "generated_project" ]; then
   sudo npm install --quiet -g nsp nsp-preprocessor-yarn npm-check ejs > /dev/null
   cd "$(dirname "$0")"
+  npm install --quiet ejs > /dev/null
   mkdir generated_project
   node generate_package.js
   cd generated_project
   yarn
-  nsp check --preprocessor yarn
-  npm-check -s
+  nsp check --preprocessor yarn || (exit 0)
+  npm-check -s || (exit 0)
 else
   echo 'Bad value of $TEST_VERSIONS'
   exit 1
