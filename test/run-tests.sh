7371fa95a3b96476729ddfdbc8f00d92c93d50e4
Jakub Bogucki <jakub300@gmail.com>
2017-11-20 19:17:19 +0100

    Improve caching, add versions test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,21 +6,17 @@ if [ "$TEST_VERSIONS" == "" ]; then
   eslint .
   mocha test/**/*.test.js -b --reporter spec
 elif [ "$TEST_VERSIONS" == "generator" ]; then
-  rm -rf node_modules
-  npm install --quiet > /dev/null
-  npm install --quiet -g nsp npm-check > /dev/null
-  npm shrinkwrap --dev
-  nsp check
+  sudo npm install --quiet -g nsp nsp-preprocessor-yarn npm-check > /dev/null
+  nsp check --preprocessor yarn
   npm-check -s
 elif [ "$TEST_VERSIONS" == "generated_project" ]; then
-  npm install --quiet -g nsp npm-check ejs > /dev/null
+  sudo npm install --quiet -g nsp nsp-preprocessor-yarn npm-check ejs > /dev/null
   cd "$(dirname "$0")"
   mkdir generated_project
   node generate_package.js
   cd generated_project
-  npm install --quiet > /dev/null
-  npm shrinkwrap --dev
-  nsp check
+  yarn
+  nsp check --preprocessor yarn
   npm-check -s
 else
   echo 'Bad value of $TEST_VERSIONS'
